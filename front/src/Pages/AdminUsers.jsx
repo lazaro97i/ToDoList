69a2a0231b2c832e2878c74c9da551dfd152a196
Lazaro97i <lazarodelprado97@gmail.com>
2024-07-23 07:04:30 -0300

    correcciones

--- front/src/Pages/AdminUsers.jsx
+++ front/src/Pages/AdminUsers.jsx
@@ -26,15 +26,19 @@ const AdminUsers = () => {
 
   const getUsers = async()=>{
     try{
+      setLoading(true)
       const res = await dispatch(getAllUsers())
     }catch(ex){
       console.log(ex)
+    }finally{
+      setLoading(false)
     }
   }
 
   const handleDelete = async()=>{
 
     try{
+      setLoading(true)
       const res = await dispatch(deleteOne(userId))
       console.log(res)
       if(res?.payload?.success){
@@ -44,6 +48,8 @@ const AdminUsers = () => {
       }
     }catch(ex){
       console.log(ex)
+    }finally{
+      setLoading(true)
     }
   }
 
@@ -63,10 +69,13 @@ const AdminUsers = () => {
 
     const getUser = async () => {
       try {
+        setLoading(true)
         const res = await dispatch(getOne({ id: userId }))
         setUserData(res?.payload?.response)
       } catch (ex) {
         console.log(ex)
+      }finally{
+        setLoading(false)
       }
     }
 
@@ -105,14 +114,14 @@ const AdminUsers = () => {
                     <div id={`task${i}`} key={i} className="border-b border-green-500 mt-5 transition-all duration-150 p-3 rounded-t  flex items-center gap-10 justify-between">
                       <div className='flex items-center gap-10'>
                       <div>
-                        <img className='w-[80px]' src={t.photo} alt="" />
+                        <img className='w-[80px] min-w-[60px]' src={t.photo} alt="" />
                       </div>
                       <div className='flex flex-col'>
                       <strong id={`title${i}`} className="text-xl">{t.username}</strong>
                       <p id={`desc${i}`} className=" opacity-60">{t.role}</p>
                       </div>
                       </div>
-                      <div className='flex gap-7'>
+                      <div className='flex gap-7 flex-col sm:flex-row h-full justify-start'>
                         <span onClick={()=>{setModalEdit(true),setUserId(t._id)}}>
                         <svg  className=' cursor-pointer' width={"30px"} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z" stroke="#22c55e" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13" stroke="#22c55e" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
                         </span>
